a37632e98dc08fa599de20376355b871799b888f
Iris Leal <iris_leal_m@hotmail.com>
2020-05-15 20:03:38 -0700

    Ready Linkedlist.__str__

--- dsa/challenges/linked_list/linked_list.py
+++ dsa/challenges/linked_list/linked_list.py
@@ -17,6 +17,16 @@ class LinkedList():
             current = current.next
         return False
 
+    def __str__(self):
+        str = ""
+        current = self.head
+
+        while current:
+            str += "{ " + current.value + " } -> "
+            current = current.next
+
+        return str + "NULL"
+
 class Node():
     def __init__ (self, value, next_ = None):
         self.value =  value
@@ -36,7 +46,8 @@ if __name__ == "__main__":
     ll.insert("Three")
     print("head", ll.head)
 
-    print(ll.includes("Three"))
+    print(ll.includes("Tdhree"))
+    print(ll.__str__())
 
 
 
