54700857f3c40c332efd795c71607df4a9f21e60
Iris Leal <iris_leal_m@hotmail.com>
2020-05-26 17:24:09 -0700

    First draft

---
+++ dsa/challenges/queue_with_stacks/queue_with_stacks.py
@@ -0,0 +1,11 @@
+from dsa.challenges.stacks_and_queues.stacks_and_queues import Stack
+
+class PseudoCode():
+    __init__()
+    main_stack = Stack()
+    temp_stack = Stack()
+
+
+if __name__ == "__main__":
+    pc = PseudoCode()
+    print("here")
