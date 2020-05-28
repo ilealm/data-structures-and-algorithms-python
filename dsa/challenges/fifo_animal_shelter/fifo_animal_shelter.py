f3629ace5c063c35187167ea172d2fe58f21b2f0
Iris Leal <iris_leal_m@hotmail.com>
2020-05-27 22:16:38 -0700

    First clases draft,  more improvements

--- dsa/challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ dsa/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,13 +1,13 @@
 
 # this class will behave as my node in the queue
 class Animal:
-    def __init__(self, type_, next_ = None):
-        self.type = type_
+    def __init__(self, kind, next_ = None):
+        self.kind = kind
         self.next = next_
 
 
     def __str__(self):
-        return f'I am an Animal object of the type {self.type}'
+        return f'I am an Animal object of the type {self.kind}.'
 
 
 
@@ -23,8 +23,8 @@ class AnimalShelter():
         return f"The rear is {self.rear} and the front is {self.front}"
 
 
-    def enqueue(self, type_):
-        new_node = Animal(type_)
+    def enqueue(self, kind):
+        new_node = Animal(kind)
 
         #is the 1st element
         if self.rear == None:
@@ -33,6 +33,7 @@ class AnimalShelter():
         else:
             self.rear.next = new_node
             self.rear = self.rear.next
+
 
 
     def dequeue(self):
@@ -44,13 +45,14 @@ class AnimalShelter():
             # if there are no more nodes, clear rear
             if self.front == None : self.rear = None
 
-            return temp_node.type
+            return temp_node.kind
         else:
             raise Exception ("Can't dequeue in an empty Queue.")
 
+
     def peek(self):
         if not self.is_empty():
-            return self.front.type
+            return self.front.kind
         else:
             raise Exception ("Can't peek in an empty Queue.")
 
@@ -65,9 +67,16 @@ if __name__ == "__main__":
     # dog = Animal('Dog')
     # print(cat, dog)
     ah = AnimalShelter()
-    # ah.enqueue('Cat')
-    ah.enqueue('Dog')
-    ah.enqueue('Cat')
-    print(ah.peek())
+    ah.enqueue('Cat1')
+    ah.enqueue('Dog2')
+    ah.enqueue('Cat3')
+    # print(ah.peek())
+    print(ah)
+    print(ah.dequeue())
+    print(ah)
+    print(ah.dequeue())
+    print(ah)
+    print(ah.dequeue())
+
 
 
