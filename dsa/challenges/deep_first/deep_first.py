5a8f7fa0d6af794dcf3b52c97a32e2bb8f74ab83
Iris Leal <iris_leal_m@hotmail.com>
2020-07-01 19:10:25 -0700

    Now ir traverse all, but not in the right order

--- dsa/challenges/deep_first/deep_first.py
+++ dsa/challenges/deep_first/deep_first.py
@@ -1,15 +1,130 @@
-from dsa.challenges.graph.graph import Graph
+# from dsa.challenges.graph.graph import Graph
 # I need to execute
 # PYTHONPATH='.' python dsa/challenges/deep_first/deep_first.py
 # to I can execute the code
 # OR in this directory: PYTHONPATH="../../../" python deep_first.py
 
+# -----------------------------------------------
+
+from collections import deque
+
+
+class Graph:
+    def __init__(self):
+        # this gives the first element in the _adjacency_list
+        # print(next(iter(graph._adjacency_list)))
+        # print(next(iter(graph._adjacency_list)).value)
+        self._adjacency_list = {}
+
+    def __str__(self):
+        return self._adjacency_list.keys()
+
+    def add_vertex(self, value):
+        new_vertex = Vertex(value)
+        # add the new vertex to the graph dictionary (vertex is the key),
+        # and asing an enpty list as value of the key
+        self._adjacency_list[new_vertex] = []
+
+        return new_vertex
+
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        if from_vertex not in self._adjacency_list:
+            raise KeyError('The vertex from must be in the graph')
+
+        if to_vertex not in self._adjacency_list:
+            raise KeyError('The vertex to must be in the graph')
+
+        new_edge = Edge(to_vertex, weight)
+
+        self._adjacency_list[from_vertex].append(new_edge) # list of neighbors
+
+
+    def get_value_obj_dictionary(self):
+        # retuns a dictionaty with obj.value = obj ?eg Arendale : <obj>
+        value_obj_objs = {}
+        objs = self.__str__()
+        for obj in objs:
+            # store city = <city object>
+            value_obj_objs[obj.value] = obj
+
+        return value_obj_objs
+
+
+    def get_vertices(self):
+        return self._adjacency_list.keys()
+
+
+    def get_neighbors(self, vertex):
+        return self._adjacency_list.get(vertex, [])
+
+
+    def __len__(self):
+        return len(self._adjacency_list)
+
+
+
+    def breadth_first(self, vertex):
+        visited_vertex = set()
+        visited_vertex_names = []
+        breadth = Queue()
+
+        breadth.enqueue(vertex)
+        visited_vertex_names.append(vertex.value)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            visited_vertex.add(front)
+
+            #  get the edges of the current vector
+            neighbors = self.get_neighbors(front)
+            for edge in neighbors:
+                if not edge.vertex in visited_vertex:
+                    visited_vertex.add(edge.vertex)
+                    visited_vertex_names.append(edge.vertex.value)
+                    breadth.enqueue(edge.vertex)
+
+        return visited_vertex_names
+
+
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+
+    # def __str__(self):
+    #     return self.value
+
+
+class Edge:
+    def __init__(self, to_vertex, weight=0):
+        self.vertex = to_vertex
+        self.weight = weight
+
+
+class Queue:
+    def __init__(self):
+        self.storage = deque()
+
+    def enqueue(self, value):
+        self.storage.appendleft(value)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.storage.pop()
+
+    def peek(self):
+        return self.storage[-1]
+
+    def is_empty(self):
+        return len(self.storage) == 0
+
+# -----------------------------------------------
 
 
 def deep_first(graph):
     visited_vertex = set()
     DFS = []
-    stack = []
+    # stack = []
     all_visited_children = False
 
     # print(graph._adjacency_list)
@@ -40,13 +155,14 @@ def deep_first(graph):
         # print("traverse", current_vertex)
 
         visited_vertex.add(current_vertex)
-        DFS.append(current_vertex)
+        DFS.append(current_vertex.value)
         edges = graph.get_neighbors(current_vertex)
 
         # print(edges)
         for i in range(0,len(edges)):
-            if not edges[i] in visited_vertex:
-                traverse(edges[i])
+            edge_convertex_to_vertex = edges[i].vertex
+            if not edge_convertex_to_vertex in visited_vertex:
+                traverse(edge_convertex_to_vertex)
 
     # 1 push the root into the stack
     root = next(iter(graph._adjacency_list))
@@ -77,8 +193,8 @@ def deep_first(graph):
     # print (edges[0].vertex.value)
     # print (edges[1].vertex.value)
 
-    print("\n\nElements in stack: ", len(stack))
-    for ele in stack:
+    print("\n\nElements in DFS: ", len(DFS))
+    for ele in DFS:
         print(ele)
 
 if __name__ == "__main__":
